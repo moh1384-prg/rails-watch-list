1a197f9543848f83971f78d608d46137a1250fee
Mohamed Ali Brahimi <mohamedali.brahimi@outlook.com>
2025-01-13 07:25:42 +0000

    Watch List

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,6 @@
 class Movie < ApplicationRecord
-  before_destroy :check_for_bookmarks
   has_many :bookmarks
+
   validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true, uniqueness: true
-end
-
-
-private
-
-def check_for_bookmarks
-  if bookmarks.any?
-    errors.add(:base, 'Cannot delete movie with bookmarks')
-    throw(:abort)
-  end
+  validates :overview, presence: true
 end
